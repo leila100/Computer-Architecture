c86621d80bfa0a08c300772bafabc9733fd9b8ce
Leila <nb.leila10@gmail.com>
2019-11-05 12:41:12 -0600

    Implement POP instruction

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -8,6 +8,7 @@ HLT  = "0001"
 ADD  = "0000"
 MULT = "0010"
 PUSH = "0101"
+POP  = "0110"
 
 class CPU:
     """Main CPU class."""
@@ -22,6 +23,7 @@ class CPU:
         self.branchtable[LDI]  = self.handle_LDI
         self.branchtable[PRN]  = self.handle_PRN
         self.branchtable[PUSH] = self.handle_PUSH
+        self.branchtable[POP] = self.handle_POP
 
     def handle_LDI(self, register, immediate):
         self.reg[register] = immediate
@@ -34,7 +36,13 @@ class CPU:
         value = self.reg[register]
         self.reg[7] -= 1
         sp = self.reg[7]
-        self.ram_write(sp, value)
+        self.ram_write(value, sp)
+    
+    def handle_POP(self, Register):
+        sp = self.reg[7]
+        value = self.ram_read(sp)
+        self.reg[Register] = value
+        self.reg[7] += 1
 
     def ram_read(self, MAR):
         MDR = self.ram[MAR]
@@ -97,7 +105,7 @@ class CPU:
         """Run the CPU."""
         running = True
         while running:
-            self.trace()            
+            # self.trace()            
 
             IR = f'{self.ram_read(self.pc):08b}'
             IR_int = int(IR, 2)
